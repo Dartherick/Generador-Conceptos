860d503fdb260df0c50e8bddf9c3aa794d2259a0
Dartherick <62523859+Dartherick@users.noreply.github.com>
2021-07-18 12:38:41 -0400

    Improving FindCategories() and FindSubCategories()

--- main.py
+++ main.py
@@ -9,15 +9,21 @@ class GeneradorConceptos():
         self.Categories = {}
         for File in os.listdir(self.Path):
             if not('.' in File):
-                self.Categories[File] = []
+                self.Categories[File] = {}
 
     def FindSubCategories(self):
         for i in self.Categories.keys():
             for File in os.listdir(os.path.join(self.Path,i)):
                 if not('.' in File):
-                    self.Categories[i].append(File)
+                    self.Categories[i][File] = []
+
+    def FindPictures(self):
+        for i in self.Categories.keys():
+            for j in self.Categories[i].keys():
+                print(j)
 
 Maldito = GeneradorConceptos('C:/Users/darth/Downloads/Trimestre 11')
 Maldito.FindCategories()
 Maldito.FindSubCategories()
 print(Maldito.Categories)
+#Maldito.FindPictures()
